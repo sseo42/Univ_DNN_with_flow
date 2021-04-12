e3dfc6faf0e6a5fccfe365776d7e980fb824ddbe
sseo <tjdalsemc@gmail.com>
2021-04-12 23:51:10 +0900

    layers

---
+++ layers.py
@@ -0,0 +1,58 @@
+import numpy as np
+from functions import soft_max as softmax
+from functions import cee
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dw = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        return np.dot(self.x, self.W) + self.b
+
+    def backward(self, dout):
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis= 0)
+        return np.dot(dout, self.W.T)
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x_origin):
+        self.mask = (x_origin>=0)
+        x = x_origin*self.mask
+        return x
+
+    def backward(self, dout):
+        return dout*self.mask
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        self.out = 1 / (1 + np.exp(-x))
+        return self.out
+
+    def backward(self, dout):
+        return dout * self.out * (1 - self.out)
+
+class Softmax_with_cee:
+    def __init__(self):
+        self.y = None
+        self.t = None
+        self.loss = None
+
+    def forward(self, x, t):
+        self.y = softmax(x)
+        self.t = t
+        self.loss = cee(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout = 1):
+        return dout * (self.y - self.t) / self.y.shape[0]
