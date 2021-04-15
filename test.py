eef5d16be7ecabc053ea5e99b2d14c8fbef42e99
sseo <tjdalsemc@gmail.com>
2021-04-15 13:31:22 +0900

    update tester

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import node
 import numpy as np
 
-test_network = node.Flow_net()
+test_network = node.Flow_net(3, 2)
 
 test_x = np.random.randn(3, 3)
 test_y = np.zeros((3, 2))
 for i in test_y:
     i[1] = 1
-test_network.update(test_x, test_y)
+
+for _ in range(10):
+    test_network.update(test_x, test_y)
